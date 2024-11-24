792d28239eaa1ac39df4339139bcbf3a89f03d26
Mariam Agboola <mariahhm42@gmail.com>
2024-11-23 19:32:44 -0700

    included header partials inside urls_index.ejs and urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -8,16 +8,24 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
+//routes for urls
 app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
   });
-  
+   
 app.get("/", function(req, res) { // a callback function that takes in two parameters: a request (req), and a response (res).
   res.send("Hello!");
 });
 
+app.get("/urls/:id", (req, res) => {
+    const templateVars = { id: req.params.id, 
+        longURL: urlDatabase[req.params.id] }; //Retrieve the long URL for the given short URL ID
+    res.render("urls_show", templateVars);
+  });
+  
+
+  
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
@@ -27,6 +35,7 @@ app.get("/urls.json", (req, res) => {
     res.render("hello_world", templateVars);
   });
 
+//starts the server
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+  console.log(`Tiny app listening on port ${PORT}!`);
+}); 
