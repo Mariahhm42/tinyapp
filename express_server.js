9f871e9710f726bbc48d0196fbdaad304e0480d3
Mariam Agboola <mariahhm42@gmail.com>
2024-11-25 02:47:13 -0700

    added the /urls/:id route

--- express_server.js
+++ express_server.js
@@ -5,37 +5,37 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs"); //sets up EJS as the templating engine
 
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
 };
-//routes for urls
+
+app.use(express.urlencoded({ extended: true }));
+
+//route handler for /urls
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
    
-app.get("/", function(req, res) { // a callback function that takes in two parameters: a request (req), and a response (res).
-  res.send("Hello!");
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
-    const templateVars = { id: req.params.id, 
-        longURL: urlDatabase[req.params.id] }; //Retrieve the long URL for the given short URL ID
-    res.render("urls_show", templateVars);
-  });
-
-app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
-  
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-
-  app.get("/hello", (req, res) => {
-    const templateVars = { greeting: "Hello World!" };
-    res.render("hello_world", templateVars);
-  });
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id] // Retrieves the long URL using the id
+  };
+  res.render("urls_show", templateVars);
+});
 
 //starts the server
 app.listen(PORT, () => {
